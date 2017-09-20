922c6b3ef923613d87f11b04c3e59090e1f66888
Ray Donnelly <mingw.android@gmail.com>
2017-09-20 07:12:08 -0700

    Fix breakage caused by CFLAGS and CXXFLAGS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,21 @@
+set PATH=%PREFIX%\cmake-bin\bin;%PATH%
+
 REM Configure step
 copy jconfig.vc jconfig.h
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
+
+mkdir %c_compiler%
+pushd %c_compiler%
+set CXXFLAGS=
+set CFLAGS=
 
 REM Build step
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release .
-if errorlevel 1 exit 1
+cmake -G "NMake Makefiles"                     ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+      -DCMAKE_BUILD_TYPE=Release               ^
+      ..
 
-nmake
-if errorlevel 1 exit 1
-
-REM Install step
-nmake install
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
+cmake --build . --config Release --target INSTALL -- VERBOSE=1
+if errorlevel 1 exit /b 1
+popd
